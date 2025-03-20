e8efe1bb77a7055b5df5414bc690458396cbce2e
freeayoub <gayoub2009@gmail.com>
2025-03-20 21:52:34 +0100

    organistion de structure backend et creer dossier frontend

--- middlewares/validationUserMiddleware.js
+++
@@ -1,9 +0,0 @@
-const { userValidationSchema } = require('../utils/validators');
-
-function validationUserMiddleware(req, res, next) {
-    userValidationSchema.validate(req.body)
-        .then(() => next())
-        .catch(err => res.status(400).json({ error: err.errors }));
-}
-
-module.exports = validationUserMiddleware;
